0e75194eaa0b693c8c141e2bbb24e833db5a6fe8
Jeffrey Huang <jeffreyhuang0331@gmail.com>
2018-11-02 14:49:47 +0800

    Add route parameter component

---
+++ pages/posts/_id.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Post :post="post" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Post from '@/components/Post'
+
+export default {
+  components: {
+    Post
+  },
+  data () {
+    return {
+      post: null
+    }
+  },
+  async asyncData ({ params }) {
+    let response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+
+    return {
+      post: response.data
+    }
+  }
+}
+</script>
